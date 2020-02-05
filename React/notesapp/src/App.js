8ad164e22e94367aa91c1af10fc1c7db90f01598
David Bolton <smdeox@live.com>
2020-02-05 12:54:59 -0500

    yes

---
+++ React/notesapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NotesApp from './NotesApp'
+
+function App() {
+  return (
+    <NotesApp />
+  );
+}
+
+export default App;
