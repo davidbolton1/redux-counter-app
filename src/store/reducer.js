c72fcb0fabccec20f7e3eb058c25959258ba25fa
David Bolton <smdeox@live.com>
2020-02-11 17:23:18 -0500

    Outsourced action types

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from './actions'
+
 const initialState = {
     counter: 0,
     results: []
@@ -5,33 +7,33 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
-		case 'INCREMENT':
+		case actionTypes.INCREMENT:
 			return {
                 ...state,
 				counter: state.counter + 1
 			};
-		case 'DECREMENT':
+		case actionTypes.DECREMENT:
 			return {
                 ...state,
 				counter: state.counter - 1
 			};
-		case 'ADD':
+		case actionTypes.ADD:
 			return {
                 ...state,
 				counter: state.counter + action.value
 			};
-		case 'SUBTRACT':
+		case actionTypes.SUBTRACT:
 			return {
                 ...state,
 				counter: state.counter - action.value
             };
-        case 'STORE_RESULT':
+        case actionTypes.STORE_RESULT:
             return {
                 ...state,
                 results: state.results.concat({id: new Date(), value: state.counter})
                 //use concat to return new array instead of push
             };
-        case 'DELETE_RESULT':
+        case actionTypes.DELETE_RESULT:
             //const newArray = [...state.results]
             //const updatedArray = state.results.filter((result, index) => index !== id);
             const updatedArray = state.results.filter(result => result.id !== action.resultElId);
