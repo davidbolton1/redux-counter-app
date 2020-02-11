3a581e2c3ff56d9834baf77352df365ec64e3456
David Bolton <smdeox@live.com>
2020-02-11 17:07:34 -0500

    Added delete logic

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -25,12 +25,21 @@ const reducer = (state = initialState, action) => {
                 ...state,
 				counter: state.counter - action.value
             };
-        case 'STORE_RESUT':
+        case 'STORE_RESULT':
             return {
                 ...state,
                 results: state.results.concat({id: new Date(), value: state.counter})
                 //use concat to return new array instead of push
             };
+        case 'DELETE_RESULT':
+            //const newArray = [...state.results]
+            //const updatedArray = state.results.filter((result, index) => index !== id);
+            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+            // Returns true for all elements where the id is not equal to the id passed with the action
+        return {
+            ...state,
+            results: updatedArray
+        }
 	}
 
 	return state;
