60e51efe76d77874bd91fbb0a7c5ec7d87e6dfce
David Bolton <smdeox@live.com>
2020-02-11 16:57:37 -0500

    Added a way to store results dynamically

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -7,20 +7,30 @@ const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case 'INCREMENT':
 			return {
+                ...state,
 				counter: state.counter + 1
 			};
 		case 'DECREMENT':
 			return {
+                ...state,
 				counter: state.counter - 1
 			};
 		case 'ADD':
 			return {
+                ...state,
 				counter: state.counter + action.value
 			};
 		case 'SUBTRACT':
 			return {
+                ...state,
 				counter: state.counter - action.value
-			};
+            };
+        case 'STORE_RESUT':
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
+                //use concat to return new array instead of push
+            };
 	}
 
 	return state;
